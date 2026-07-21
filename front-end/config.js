7a622701397d211c197a754bdf4f492f668ea33a
Jean Pedroso <jeancarlocaleffipedroso713@gmail.com>
2026-07-20 21:16:16 -0300

    Ligando modo de produção no front-end

--- front-end/config.js
+++ front-end/config.js
@@ -3,13 +3,13 @@
 
 const ENV = {
     // Altere para true quando fizer o deploy na Vercel
-    IS_PRODUCTION: false, 
+    IS_PRODUCTION: true, 
     
     // URL da API quando rodando localmente no seu computador (com Node)
     LOCAL_API_URL: 'http://localhost:3000/api',
     
     // URL da API oficial quando estiver hospedada no Render (ex: https://agenda-backend.onrender.com/api)
-    PRODUCTION_API_URL: 'URL_DO_RENDER_AQUI/api'
+    PRODUCTION_API_URL: 'https://tcc-agenda-web.onrender.com/api'
 };
 
 // Variável global que os outros arquivos vão usar
