b6bcbcb7e349a6c7fb8a5779d66c85e89d2627a7
Jean Pedroso <jeancarlocaleffipedroso713@gmail.com>
2026-07-23 19:10:21 -0300

    chore: Expoe mensagem de erro real do backend para o frontend para facilitar o debug da troca de senha

--- back-end/controllers/authController.js
+++ back-end/controllers/authController.js
@@ -194,7 +194,7 @@ exports.requestPasswordChange = async (req, res) => {
         res.json({ mensagem: 'Código de verificação enviado para o seu e-mail.' });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ erro: 'Erro ao solicitar troca de senha.' });
+        res.status(500).json({ erro: 'Erro ao solicitar troca de senha: ' + error.message });
     }
 };
 
@@ -223,6 +223,6 @@ exports.verifyPasswordChange = async (req, res) => {
         res.json({ mensagem: 'Senha alterada com sucesso!' });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ erro: 'Erro ao alterar a senha.' });
+        res.status(500).json({ erro: 'Erro ao alterar a senha: ' + error.message });
     }
 };
