f3fdc9366f5b57e1cf3cd8bb255edefdfb46f49b
Jean Pedroso <jeancarlocaleffipedroso713@gmail.com>
2026-08-16 19:13:03 -0300

    fix: corrigir import do authMiddleware

--- back-end/routes/gruposRoutes.js
+++ back-end/routes/gruposRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const gruposController = require('../controllers/gruposController');
-const { authMiddleware } = require('../middlewares/authMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // Rotas protegidas por autenticação
 router.use(authMiddleware);
